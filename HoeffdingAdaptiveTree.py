7559c8815d0e7790c20295470e816f030ef6e063
wilfriedzakie <zakie.willy@gmail.com>
2018-02-13 16:28:00 +0100

    Update HoeffdingAdaptiveTree.py

--- HoeffdingAdaptiveTree.py
+++ HoeffdingAdaptiveTree.py
@@ -11,6 +11,26 @@ import numpy as np
 
 class HoeffdingAdaptiveTree(HoeffdingTree):
 
+    """ Hoeffding AdaptiveTree 
+
+    A Hoeffding Adaptive tree is a decision tree-like algorithm which extends Hoeffding tree algorithm. It's used for learning incrementally from data streams. 
+    It grows tree as is done by the the Hoeffding Tree Algorithm and has also as mathematical guarantee the Hoeffding bound. 
+
+    It builds alternate trees whenever a some changes in the distribution is noticed by ADWIN. Unlike other adaptives window tree Algorithms, such as Hoeffding tree window, it does not need a fixed size of window before to raise an alarm and statistics are store in nodes which decides the number of sufficient examples it needs to split.  The alternate tree can promoted if the tree decrease in accuracy compared to the alternate one.
+
+        See for details:
+        https://link.springer.com/chapter/10.1007r%2F978-3-642-03915-7_22
+
+        Implementation based on:
+        MOA: Massive Online Analysis; Journal of Machine Learning Research 11: 1601-1604
+
+    Parameters
+    ----------
+
+
+    Implementation based on MOA
+    """
+
 
     class NewNode(metaclass= ABCMeta):
 
@@ -85,7 +105,7 @@ class HoeffdingAdaptiveTree(HoeffdingTree):
                         byteSize += child.calc_byte_size_including_subtree()
             return byteSize
 
-        #Valid +1
+        
         def number_leaves(self):
             numleaves = 0
             for child in self._children:
@@ -93,7 +113,6 @@ class HoeffdingAdaptiveTree(HoeffdingTree):
                     numleaves += child.number_leaves()
             return numleaves
 
-        #valid
         def getErrorEstimation(self):
             return self.estimationErrorWeight._estimation
 
@@ -210,8 +229,6 @@ class HoeffdingAdaptiveTree(HoeffdingTree):
 
         #validated ##Check if mistake with observed class distribution
         def filterInstanceToLeaves(self, X, myparent, parentBranch,updateSplitterCounts,foundNodes=None):
-           # if foundNodes is None:
-           #     foundNodes=[]
 
             #if updateSplitterCounts:#Maybe
             #    self._observed_class_distribution[y] = 0.0
